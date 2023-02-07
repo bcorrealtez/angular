15550dfc93877b9da2087d48fe1aa9cdac078215
bcorrea <brunocorreaaltez2016@gmail.com>
2023-02-07 16:10:39 -0300

    first routes

--- prueba/src/app/app-routing.module.ts
+++ prueba/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoriesComponent } from './components/categories/categories.component';
-
 import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'categories' , component: CategoriesComponent},
-  {path: '' , component: HomeComponent},
-  {path: '' , component: HomeComponent},
-  {path: '' , component: HomeComponent},
+  {path: 'cart' , component: CartComponent},
+  {
+    path: 'profile' ,
+    component: ProfileComponent,
+    children: [
+      { path: ':category', component: ProductsComponent }
+    ]
+  },
+  
 ];
 
 @NgModule({
